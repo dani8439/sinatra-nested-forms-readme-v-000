a50199c5c4ae5046422ddce7e97b4f7bb5a0cccc
Dani Schuhman <dani8439@aol.com>
2018-08-27 16:11:14 +0000

    Done.

---
+++ app.rb
@@ -0,0 +1,20 @@
+require_relative 'models/course.rb'
+require_relative 'models/student.rb'
+
+class Application
+
+  get '/' do
+  erb :new
+  end
+
+  post '/student' do
+  @student = Student.new(params[:student])
+
+  params[:student][:courses].each do |details|
+    Course.new(details)
+  end
+
+  @courses = Course.all
+
+  erb :student
+end
